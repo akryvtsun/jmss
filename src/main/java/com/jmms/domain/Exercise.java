c274d3efc7055734409d0624bf668a8dfee886e5
englishman <kontiky@gmail.com>
2016-06-03 14:42:28 -0400

    #4 create Exercise class

---
+++ src/main/java/com/jmms/domain/Exercise.java
@@ -0,0 +1,9 @@
+package com.jmms.domain;
+
+public final class Exercise {
+    private final int number;
+
+    public Exercise(int number) {
+        this.number = number;
+    }
+}
--- src/main/java/com/jmms/domain/Match.java
+++ src/main/java/com/jmms/domain/Match.java
@@ -2,9 +2,11 @@ package com.jmms.domain;
 
 public final class Match {
     private final String name;
+    private final Exercise[] exercises;
 
-    public Match(String name) {
+    public Match(String name, Exercise[] exercises) {
         this.name = name;
+        this.exercises = exercises;
     }
 
     public String getName() {
