7825cdd60d998143cd66ce5dbd5da0b2cca5fc03
englishman <kontiky@gmail.com>
2016-06-13 18:06:34 -0400

    #14 rework Competitors window

--- src/main/java/com/jmms/application/CompetitorsWindow.java
+++ src/main/java/com/jmms/application/CompetitorsWindow.java
@@ -1,79 +1,42 @@
 package com.jmms.application;
 
 import com.jmms.domain.Member;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
 
-public class CompetitorsWindow extends BorderPane {
+public class CompetitorsWindow extends HBox {
 
-    private final ObservableList<Member> membersData;
-    private final ObservableList<Member> data;
-
-    private final TabPane tabPane = new TabPane();
+    private final ObservableList<Member> members;
+    private final ObservableList<Member> competitors;
 
     private final TableView<Member> membersTable = new TableView<>();
-    private final TableView<Member> table = new TableView<>();
+    private final TableView<Member> competitorsTable = new TableView<>();
 
     public CompetitorsWindow(List<Member> members, List<Member> competitors) {
-        membersData = FXCollections.observableList(members);
-        data = FXCollections.observableList(competitors);
+        this.members = FXCollections.observableList(members);
+        this.competitors = FXCollections.observableList(competitors);
 
-        setCenter(createTabPane());
-        setRight(createButtonPane());
+        getChildren().addAll(createMembersPane(), createButtonsPane(), createCompetitorsPane());
     }
 
-    private TabPane createTabPane() {
-        Tab competitorTab = new Tab("Competitor");
-        competitorTab.setContent(createCompetitorTab());
-
-        Tab competitorListTab = new Tab("Competitor List");
-        competitorListTab.setContent(createCompetitorListTab());
-
-        tabPane.getSelectionModel().selectedItemProperty().addListener(
-                new ChangeListener<Tab>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) {
-                        TableView.TableViewSelectionModel<Member> tableSelectionModel = table.getSelectionModel();
-                        int index = tableSelectionModel.getSelectedIndex();
-//                        if (index >= 0) {
-//                            if (competitorListTab.equals(newValue)) {
-//                                Member member = new Member(firstNameField.getText(), lastNameField.getText());
-//
-//                                Data.set(index, member);
-//                            } else if (competitorTab.equals(newValue)) {
-//                                Member member = Data.get(index);
-//
-//                                firstNameField.setText(member.getFirstName());
-//                                lastNameField.setText(member.getLastName());
-//                            }
-//                        }
-                    }
-                }
-        );
-        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-
-        tabPane.getTabs().add(competitorTab);
-        tabPane.getTabs().add(competitorListTab);
-
-        return tabPane;
-    }
-
-    private Pane createCompetitorTab() {
+    private Pane createMembersPane() {
         BorderPane pane = new BorderPane();
         pane.setPadding(new Insets(10, 10, 10, 10));
+
+        pane.setTop(new Label("Members List"));
 
         TableColumn firstNameCol = new TableColumn("First Name");
         firstNameCol.setCellValueFactory(new PropertyValueFactory<Member, String>("firstName"));
@@ -82,36 +45,50 @@ public class CompetitorsWindow extends BorderPane {
         lastNameCol.setCellValueFactory(new PropertyValueFactory<Member, String>("lastName"));
 
         membersTable.setEditable(true);
-        membersTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-//                if (event.getClickCount() > 1) {
-//                    TableView.TableViewSelectionModel<Member> tableSelectionModel = table.getSelectionModel();
-//                    int index = tableSelectionModel.getSelectedIndex();
-//                    if (index >= 0) {
-//                        Member member = data.get(index);
-//
-//                        firstNameField.setText(member.getFirstName());
-//                        lastNameField.setText(member.getLastName());
-//
-//                        SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
-//                        selectionModel.select(0);
-//                    }
-//                }
-            }
-        });
-
         membersTable.getColumns().addAll(firstNameCol, lastNameCol);
-        membersTable.setItems(membersData);
+        membersTable.setItems(members);
 
         pane.setCenter(membersTable);
 
         return pane;
     }
 
-    private Pane createCompetitorListTab() {
+    private Node createButtonsPane() {
+        final VBox pane = new VBox();
+        pane.setSpacing(5);
+        pane.setPadding(new Insets(40, 10, 10, 10));
+
+        Button aNew = new Button("Add >>");
+        aNew.setMaxWidth(Double.MAX_VALUE);
+        aNew.setOnAction(e -> {
+            TableView.TableViewSelectionModel<Member> tableSelectionModel = membersTable.getSelectionModel();
+            int index = tableSelectionModel.getSelectedIndex();
+            if (index >= 0) {
+                Member member = members.remove(index);
+                competitors.add(member);
+            }
+        });
+
+        Button delete = new Button("<< Delete");
+        delete.setMaxWidth(Double.MAX_VALUE);
+        delete.setOnAction(e -> {
+            TableView.TableViewSelectionModel<Member> tableSelectionModel = competitorsTable.getSelectionModel();
+            int index = tableSelectionModel.getSelectedIndex();
+            if (index >= 0) {
+                Member member = competitors.remove(index);
+                members.add(member);
+            }
+        });
+
+        pane.getChildren().addAll(aNew, delete);
+        return pane;
+    }
+
+    private Pane createCompetitorsPane() {
         BorderPane pane = new BorderPane();
         pane.setPadding(new Insets(10, 10, 10, 10));
+
+        pane.setTop(new Label("Competitors List"));
 
         TableColumn firstNameCol = new TableColumn("First Name");
         firstNameCol.setCellValueFactory(new PropertyValueFactory<Member, String>("firstName"));
@@ -119,59 +96,12 @@ public class CompetitorsWindow extends BorderPane {
         TableColumn lastNameCol = new TableColumn("Last Name");
         lastNameCol.setCellValueFactory(new PropertyValueFactory<Member, String>("lastName"));
 
-        table.setEditable(true);
-        table.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-//                if (event.getClickCount() > 1) {
-//                    TableView.TableViewSelectionModel<Member> tableSelectionModel = table.getSelectionModel();
-//                    int index = tableSelectionModel.getSelectedIndex();
-//                    if (index >= 0) {
-//                        Member member = data.get(index);
-//
-//                        firstNameField.setText(member.getFirstName());
-//                        lastNameField.setText(member.getLastName());
-//
-//                        SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
-//                        selectionModel.select(0);
-//                    }
-//                }
-            }
-        });
+        competitorsTable.setEditable(true);
+        competitorsTable.getColumns().addAll(firstNameCol, lastNameCol);
+        competitorsTable.setItems(competitors);
 
-        table.getColumns().addAll(firstNameCol, lastNameCol);
-        table.setItems(data);
+        pane.setCenter(competitorsTable);
 
-        pane.setCenter(table);
-
-        return pane;
-    }
-
-    private Node createButtonPane() {
-        final VBox pane = new VBox();
-        pane.setSpacing(5);
-        pane.setPadding(new Insets(40, 10, 10, 10));
-
-        Button aNew = new Button("New");
-        aNew.setMaxWidth(Double.MAX_VALUE);
-        aNew.setOnAction(e -> {
-//            Member match = new Member(firstNameField.getText(), lastNameField.getText());
-//
-//            Data.add(match);
-//
-//            firstNameField.clear();
-//            lastNameField.clear();
-        });
-
-        Button delete = new Button("Delete");
-        delete.setMaxWidth(Double.MAX_VALUE);
-        delete.setOnAction(e -> {
-            int focusedIndex = table.getSelectionModel().getFocusedIndex();
-            if (focusedIndex >= 0)
-                data.remove(focusedIndex);
-        });
-
-        pane.getChildren().addAll(aNew, delete);
         return pane;
     }
 }
