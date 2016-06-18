6c3f10a7e8ecb9fb2745e7d401525580dd0eca5e
englishman <kontiky@gmail.com>
2016-06-18 00:07:33 -0400

    #14 components behaviour updates

--- src/main/java/com/jmms/application/ScoringWindow.java
+++ src/main/java/com/jmms/application/ScoringWindow.java
@@ -45,10 +45,27 @@ public class ScoringWindow extends GridPane {
     public ScoringWindow(List<Match> matches) {
         this.matches = matches;
 
+        prepareComponents();
+
         setPadding(new Insets(10, 10, 10, 10));
         setHgap(10);
         setVgap(10);
 
+        add(createCoordinatesPane(), 0, 0);
+        add(createScoringPane(), 1, 0);
+
+        add(createPenaltiesPane(), 0, 1);
+        add(createTimePane(), 1, 1);
+
+        add(createButtonsPane(), 0, 2, 2, 1);
+
+        if (!matches.isEmpty()) {
+            Match match = matches.get(0);
+            matchComboBox.setValue(match);
+        }
+    }
+
+    private void prepareComponents() {
         matchComboBox = createMatchComboBox(matches);
         stageComboBox = createStageComboBox();
         competitorComboBox = createCompetitorComboBox();
@@ -60,221 +77,7 @@ public class ScoringWindow extends GridPane {
         misses = createSpinner();
         procedurals = createSpinner();
 
-        time = new TextField();
-
-        //matchComboBox.setValue(matches.get(0));
-
-        add(createCoordinatesPane(), 0, 0);
-        add(createScoringPane(), 1, 0);
-
-        add(createPenaltiesPane(), 0, 1);
-        add(createTimePane(), 1, 1);
-
-        add(createButtonsPane(), 0, 2, 2, 1);
-    }
-
-    private ComboBox<Match> createMatchComboBox(List<Match> matches) {
-        ComboBox<Match> comboBox = new ComboBox<>(FXCollections.observableList(matches));
-        comboBox.setConverter(new StringConverter<Match>() {
-            @Override
-            public String toString(Match object) {
-                return object.getName();
-            }
-
-            @Override
-            public Match fromString(String string) {
-                throw new UnsupportedOperationException();
-            }
-        });
-        comboBox.setOnAction(event -> {
-            Match match = comboBox.getValue();
-
-            List<Stage> stages = match.getStages();
-            stageComboBox.setItems(FXCollections.observableList(stages));
-            stageComboBox.setValue(stages.get(0));
-
-            List<Member> competitors = match.getCompetitors();
-            competitorComboBox.setItems(FXCollections.observableList(competitors));
-            competitorComboBox.setValue(competitors.get(0));
-        });
-        return comboBox;
-    }
-
-    private ComboBox<Stage> createStageComboBox() {
-        ComboBox<Stage> comboBox = new ComboBox<>();
-        comboBox.setConverter(new StringConverter<Stage>() {
-            @Override
-            public String toString(Stage object) {
-                return String.valueOf(object.getNumber());
-            }
-
-            @Override
-            public Stage fromString(String string) {
-                throw new UnsupportedOperationException();
-            }
-        });
-        comboBox.setOnAction(event -> {
-            updatePassingComponents();
-        });
-        return comboBox;
-    }
-
-    private ComboBox<Member> createCompetitorComboBox() {
-        ComboBox<Member> comboBox = new ComboBox<>();
-        comboBox.setConverter(new StringConverter<Member>() {
-            @Override
-            public String toString(Member object) {
-                return String.format("%s, %s", object.getLastName(), object.getFirstName());
-            }
-
-            @Override
-            public Member fromString(String string) {
-                throw new UnsupportedOperationException();
-            }
-        });
-        comboBox.setOnAction(event -> {
-            updatePassingComponents();
-        });
-        return comboBox;
-    }
-
-    private void updatePassingComponents() {
-        Match value = matchComboBox.getValue();
-        Map<Stage, Map<Member, Passing>> results = value.getResults();
-
-        Stage stage = stageComboBox.getValue();
-        Map<Member, Passing> map = results.get(stage);
-
-        if (map == null) {
-            map = new HashMap<>();
-        }
-
-        Member member = competitorComboBox.getValue();
-        Passing passing = map.get(member);
-
-        if (passing == null) {
-            passing = new Passing(0, 0, 0, 0, 0, 0);
-        }
-
-        aHits.getValueFactory().setValue(passing.getAlphas());
-        cHits.getValueFactory().setValue(passing.getCharlies());
-        dHits.getValueFactory().setValue(passing.getDeltas());
-
-        misses.getValueFactory().setValue(passing.getMisses());
-        procedurals.getValueFactory().setValue(passing.getPenalties());
-
-        time.setText(String.valueOf(passing.getTime()));
-    }
-
-    private TitledPane createCoordinatesPane() {
-
-        GridPane pane = new GridPane();
-        //pane.setStyle("-fx-background-color: #cccccc; -fx-border-color: #464646; ");
-        //pane.setPrefWidth(Double.MAX_VALUE);
-        pane.setHgap(5);
-        pane.setVgap(5);
-
-        pane.add(new Label("Match:"), 0,0);
-        pane.add(matchComboBox, 1,0);
-
-        pane.add(new Label("Stage:"), 0,1);
-        pane.add(stageComboBox, 1,1);
-
-        pane.add(new Label("Competitor:"), 0,2);
-        pane.add(competitorComboBox, 1,2);
-
-        TitledPane p = new TitledPane("Coordinates", pane);
-        p.setCollapsible(false);
-
-        return p;
-    }
-
-    private TitledPane createScoringPane() {
-        GridPane pane = new GridPane();
-        pane.setHgap(5);
-        pane.setVgap(5);
-        //pane.setStyle("-fx-background-color: orange; -fx-border-color: #464646; ");
-
-        pane.add(new Label("A Hits:"), 0,0);
-        pane.add(aHits, 1,0);
-
-        pane.add(new Label("C Hits:"), 0,1);
-        pane.add(cHits, 1,1);
-
-        pane.add(new Label("D Hits:"), 0,2);
-        pane.add(dHits, 1,2);
-
-        TitledPane p = new TitledPane("Scoring", pane);
-        p.setCollapsible(false);
-        p.setMaxHeight(Double.MAX_VALUE);
-
-        return p;
-    }
-
-    private Node createPenaltiesPane() {
-        GridPane pane = new GridPane();
-        pane.setHgap(5);
-        pane.setVgap(5);
-        //pane.setStyle("-fx-background-color: orange; -fx-border-color: #464646; ");
-
-        pane.add(new Label("Misses:"), 0,0);
-        pane.add(misses, 1,0);
-
-        pane.add(new Label("Procedurals:"), 0,1);
-        pane.add(procedurals, 1,1);
-
-        TitledPane p = new TitledPane("Penalties", pane);
-        p.setCollapsible(false);
-        p.setMaxHeight(Double.MAX_VALUE);
-
-        return p;
-    }
-
-    private TitledPane createTimePane() {
-        GridPane pane = new GridPane();
-        pane.setHgap(5);
-        pane.setVgap(5);
-
-        pane.add(new Label("Total time"), 0,0);
-        pane.add(time, 1,0);
-
-        TitledPane p = new TitledPane("Time", pane);
-        p.setCollapsible(false);
-        p.setMaxHeight(Double.MAX_VALUE);
-
-        return p;
-    }
-
-    private Spinner createSpinner() {
-        Spinner<Integer> spinner = new Spinner<>();
-        spinner.setEditable(true);
-        SpinnerValueFactory svf =
-                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 99);
-        spinner.setValueFactory(svf);
-        spinner.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
-        return spinner;
-    }
-
-    private Pane createButtonsPane() {
-        BorderPane pane = new BorderPane();
-        Button button = new Button("Confirm");
-        button.setOnAction(event -> {
-            Passing passing = new Passing(aHits.getValue(), cHits.getValue(), dHits.getValue(),
-                    misses.getValue(), procedurals.getValue(),
-                    Double.parseDouble(time.getText()));
-
-            Match match = matchComboBox.getValue();
-            Map<Member, Passing> map = match.getResults().get(stageComboBox.getValue());
-
-            if (map == null) {
-                map = new HashMap<>();
-                match.getResults().put(stageComboBox.getValue(), map);
-            }
-
-            map.put(competitorComboBox.getValue(), passing);
-        });
-        pane.setRight(button);
-        return pane;
+        time = new TextField("0.00");
     }
 
     public static void main(String[] args) {
@@ -332,7 +135,8 @@ public class ScoringWindow extends GridPane {
         ScoringWindow window = new ScoringWindow(matches);
 
         Platform.runLater(new Runnable() {
-            @Override public void run() {
+            @Override
+            public void run() {
                 javafx.stage.Stage stage = new javafx.stage.Stage();
                 stage.setTitle("Rapid Scoring");
                 Scene scene = new Scene(window);
@@ -343,5 +147,211 @@ public class ScoringWindow extends GridPane {
                 stage.show();
             }
         });
+    }
+
+    private ComboBox<Match> createMatchComboBox(List<Match> matches) {
+        ComboBox<Match> comboBox = new ComboBox<>(FXCollections.observableList(matches));
+        comboBox.setConverter(new StringConverter<Match>() {
+            @Override
+            public String toString(Match object) {
+                return object.getName();
+            }
+
+            @Override
+            public Match fromString(String string) {
+                throw new UnsupportedOperationException();
+            }
+        });
+        comboBox.valueProperty().addListener(e -> {
+            Match match = comboBox.getValue();
+
+            List<Stage> stages = match.getStages();
+            stageComboBox.setItems(FXCollections.observableList(stages));
+            stageComboBox.getSelectionModel().select(0);
+
+            List<Member> competitors = match.getCompetitors();
+            competitorComboBox.setItems(FXCollections.observableList(competitors));
+            competitorComboBox.getSelectionModel().select(0);
+        });
+        return comboBox;
+    }
+
+    private ComboBox<Stage> createStageComboBox() {
+        ComboBox<Stage> comboBox = new ComboBox<>();
+        comboBox.setConverter(new StringConverter<Stage>() {
+            @Override
+            public String toString(Stage object) {
+                return String.valueOf(object.getNumber());
+            }
+
+            @Override
+            public Stage fromString(String string) {
+                throw new UnsupportedOperationException();
+            }
+        });
+        comboBox.valueProperty().addListener(e -> {
+            updatePassingComponents();
+        });
+        return comboBox;
+    }
+
+    private ComboBox<Member> createCompetitorComboBox() {
+        ComboBox<Member> comboBox = new ComboBox<>();
+        comboBox.setConverter(new StringConverter<Member>() {
+            @Override
+            public String toString(Member object) {
+                return String.format("%s, %s", object.getLastName(), object.getFirstName());
+            }
+
+            @Override
+            public Member fromString(String string) {
+                throw new UnsupportedOperationException();
+            }
+        });
+        comboBox.valueProperty().addListener(e -> {
+            updatePassingComponents();
+        });
+        return comboBox;
+    }
+
+    private void updatePassingComponents() {
+        Match value = matchComboBox.getValue();
+        Map<Stage, Map<Member, Passing>> results = value.getResults();
+
+        Stage stage = stageComboBox.getValue();
+        Map<Member, Passing> map = results.get(stage);
+
+        if (map == null) {
+            map = new HashMap<>();
+            results.put(stage, map);
+        }
+
+        Member member = competitorComboBox.getValue();
+        Passing passing = map.get(member);
+
+        if (passing == null) {
+            passing = new Passing(0, 0, 0, 0, 0, 0.0);
+            map.put(member, passing);
+        }
+
+        aHits.getValueFactory().setValue(passing.getAlphas());
+        cHits.getValueFactory().setValue(passing.getCharlies());
+        dHits.getValueFactory().setValue(passing.getDeltas());
+
+        misses.getValueFactory().setValue(passing.getMisses());
+        procedurals.getValueFactory().setValue(passing.getPenalties());
+
+        time.setText(String.valueOf(passing.getTime()));
+    }
+
+    private TitledPane createCoordinatesPane() {
+
+        GridPane pane = new GridPane();
+        //pane.setStyle("-fx-background-color: #cccccc; -fx-border-color: #464646; ");
+        //pane.setPrefWidth(Double.MAX_VALUE);
+        pane.setHgap(5);
+        pane.setVgap(5);
+
+        pane.add(new Label("Match:"), 0, 0);
+        pane.add(matchComboBox, 1, 0);
+
+        pane.add(new Label("Stage:"), 0, 1);
+        pane.add(stageComboBox, 1, 1);
+
+        pane.add(new Label("Competitor:"), 0, 2);
+        pane.add(competitorComboBox, 1, 2);
+
+        TitledPane p = new TitledPane("Coordinates", pane);
+        p.setCollapsible(false);
+
+        return p;
+    }
+
+    private TitledPane createScoringPane() {
+        GridPane pane = new GridPane();
+        pane.setHgap(5);
+        pane.setVgap(5);
+        //pane.setStyle("-fx-background-color: orange; -fx-border-color: #464646; ");
+
+        pane.add(new Label("A Hits:"), 0, 0);
+        pane.add(aHits, 1, 0);
+
+        pane.add(new Label("C Hits:"), 0, 1);
+        pane.add(cHits, 1, 1);
+
+        pane.add(new Label("D Hits:"), 0, 2);
+        pane.add(dHits, 1, 2);
+
+        TitledPane p = new TitledPane("Scoring", pane);
+        p.setCollapsible(false);
+        p.setMaxHeight(Double.MAX_VALUE);
+
+        return p;
+    }
+
+    private Node createPenaltiesPane() {
+        GridPane pane = new GridPane();
+        pane.setHgap(5);
+        pane.setVgap(5);
+        //pane.setStyle("-fx-background-color: orange; -fx-border-color: #464646; ");
+
+        pane.add(new Label("Misses:"), 0, 0);
+        pane.add(misses, 1, 0);
+
+        pane.add(new Label("Procedurals:"), 0, 1);
+        pane.add(procedurals, 1, 1);
+
+        TitledPane p = new TitledPane("Penalties", pane);
+        p.setCollapsible(false);
+        p.setMaxHeight(Double.MAX_VALUE);
+
+        return p;
+    }
+
+    private TitledPane createTimePane() {
+        GridPane pane = new GridPane();
+        pane.setHgap(5);
+        pane.setVgap(5);
+
+        pane.add(new Label("Total time"), 0, 0);
+        pane.add(time, 1, 0);
+
+        TitledPane p = new TitledPane("Time", pane);
+        p.setCollapsible(false);
+        p.setMaxHeight(Double.MAX_VALUE);
+
+        return p;
+    }
+
+    private Spinner createSpinner() {
+        Spinner<Integer> spinner = new Spinner<>();
+        spinner.setEditable(true);
+        SpinnerValueFactory svf =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 99);
+        spinner.setValueFactory(svf);
+        spinner.getStyleClass().add(Spinner.STYLE_CLASS_ARROWS_ON_RIGHT_HORIZONTAL);
+        return spinner;
+    }
+
+    private Pane createButtonsPane() {
+        BorderPane pane = new BorderPane();
+        Button button = new Button("Confirm");
+        button.setOnAction(event -> {
+            Passing passing = new Passing(aHits.getValue(), cHits.getValue(), dHits.getValue(),
+                    misses.getValue(), procedurals.getValue(),
+                    Double.parseDouble(time.getText()));
+
+            Match match = matchComboBox.getValue();
+            Map<Member, Passing> map = match.getResults().get(stageComboBox.getValue());
+
+            if (map == null) {
+                map = new HashMap<>();
+                match.getResults().put(stageComboBox.getValue(), map);
+            }
+
+            map.put(competitorComboBox.getValue(), passing);
+        });
+        pane.setRight(button);
+        return pane;
     }
 }
