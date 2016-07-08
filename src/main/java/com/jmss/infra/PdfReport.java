fe9ac43cc0c0aca7803e9f67de755f681f257233
englishman <kontiky@gmail.com>
2016-07-08 16:49:37 -0400

    #5 use try-with-resources statement

--- src/main/java/com/jmss/infra/PdfReport.java
+++ src/main/java/com/jmss/infra/PdfReport.java
@@ -2,11 +2,14 @@ package com.jmss.infra;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-// TODO all logging
 public final class PdfReport {
+    private static final Logger LOG = Logger.getLogger(PdfReport.class.getName());
 
     private final File output;
 
@@ -16,23 +19,20 @@ public final class PdfReport {
 
     // TODO fix Cyrillic letters visible
     // TODO add Exception(???) to method signature
-    // TODO refactor with try-for-resources
     // TODO rethink whether content should be save or ctor param?
     public void save(String content) {
-        try {
-            OutputStream file = new FileOutputStream(output);
+        LOG.info(String.format("Storing PDF into '%s'...", output.getAbsolutePath()));
+
+        try (OutputStream os = new FileOutputStream(output)) {
             Document document = new Document();
-            PdfWriter writer = PdfWriter.getInstance(document, file);
-            document.open();
-
+            PdfWriter writer = PdfWriter.getInstance(document, os);
             InputStream is = new ByteArrayInputStream(content.getBytes());
-            com.itextpdf.tool.xml.XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
 
+            document.open();
+            XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
             document.close();
-            writer.close();
-            file.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.log(Level.SEVERE, "Error while PDF file generation", e);
         }
     }
 }
