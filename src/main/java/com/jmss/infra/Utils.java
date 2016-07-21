9a7b311627d36988c9d92cfe3c966fd24710fed4
englishman <kontiky@gmail.com>
2016-07-21 18:12:51 -0400

    #27 fix logger config loading

--- src/main/java/com/jmss/infra/Utils.java
+++ src/main/java/com/jmss/infra/Utils.java
@@ -1,11 +1,13 @@
 package com.jmss.infra;
 
+import com.jmss.application.Launcher;
+
 import java.io.InputStream;
 
 public final class Utils {
 
     public static InputStream getResource(String resourceName) {
-        return Utils.class.getClassLoader().getResourceAsStream(resourceName);
+        return Utils.class.getResourceAsStream(resourceName);
     }
 
     private Utils() {}
