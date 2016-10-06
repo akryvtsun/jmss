97df775fdcd55c9be83d3b735cffd66971d8822e
englishman <kontiky@gmail.com>
2016-10-06 16:01:09 -0400

    #35 create MembersViewModel skeleton

---
+++ src/main/java/com/jmss/application/MembersViewModel.java
@@ -0,0 +1,36 @@
+package com.jmss.application;
+
+import com.jmss.domain.Member;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
+
+import java.util.List;
+
+public class MembersViewModel {
+    private /*final*/ ObjectProperty<SingleSelectionModel<Tab>> selectionModel;
+
+    private final StringProperty firstName = new SimpleStringProperty();
+    private final StringProperty lastName = new SimpleStringProperty();
+    private final ObservableList<Member> membersList;
+
+    public MembersViewModel(List<Member> members) {
+        membersList = FXCollections.observableList(members);
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public ObservableList<Member> membersProperty() {
+        return membersList;
+    }
+}
